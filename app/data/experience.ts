e157bddc654fcfa44bea7df7a707daa4e7f5f1fe
carlosdrojas <carlosrojas@Carloss-MacBook-Pro.local>
2025-10-01 03:24:37 -0500

    deployment bug with experiences

--- app/data/experience.ts
+++ app/data/experience.ts
@@ -23,7 +23,7 @@ export const experiences: Experience[] = [
       "Trained models across 4 different environments while running 50+ parallel experiments daily, leveraging Python, NumPy, and Matplotlib for data analysis.",
       "Collaborated with a team of 3 researchers to develop 5 novel algorithms in dynamic traffic simulations"
     ],
-    type: "research internship"
+    type: "internship"
   },
 
   {
